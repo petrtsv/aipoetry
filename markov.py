04cb814badcd282f7dd5fe80b2e2f6e40991db42
Petr Tsvetkov <Petr.Tsvetkov@waveaccess.ru>
2021-05-05 20:01:31 +0300

    Use user's line as an input for markov's chains when possible

--- markov.py
+++ markov.py
@@ -6,8 +6,28 @@ with open('poetry_dataset.txt', encoding='utf-8') as f:
 model = markovify.NewlineText(read_text)
 
 
-def markov_generate():
-    result = model.make_sentence()
+def markov_generate(init_state):
+    init_state = tuple((e.lower() for e in init_state))
+    result = None
+    try:
+        result = model.make_sentence(init_state=init_state)
+    except KeyError:
+        init_state = None
+
     while result is None:
-        result = model.make_sentence()
-    return result.replace('$', '\n')
+        result = model.make_sentence(init_state=init_state)
+    lines = result.split('$')
+
+    words_to_remove = init_state if init_state is not None else ()
+    pos = 0
+
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip()
+        while pos < len(words_to_remove) and lines[i].startswith(words_to_remove[pos]):
+            lines[i] = lines[i][len(words_to_remove[pos]):]
+            lines[i] = lines[i].strip()
+            pos += 1
+
+    lines = [e for e in lines if e != ""]
+
+    return '\n'.join(lines)
--- tgbot.py
+++ tgbot.py
@@ -1,8 +1,8 @@
 import logging
 import os
 
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 
 from markov import markov_generate
 
@@ -13,36 +13,45 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+keyboard = [
+    [InlineKeyboardButton("Сгенерировать заново", callback_data='generate_new'),
+     InlineKeyboardButton("Сохранить", callback_data='save')],
+]
+
+keyboard_markup = InlineKeyboardMarkup(keyboard)
+
 
 def help_handler(update: Update, context: CallbackContext):
     update.message.reply_text("Отправь мне первую строчку на русском языке и получи четверостишье, сочиненное машиной!")
 
 
-def get_keyboard_markup():
-    keyboard = [
-        [InlineKeyboardButton("Сгенерировать заново", callback_data='generate_new')],
-    ]
+def callback_handler(update: Update, context: CallbackContext):
+    query = update.callback_query
 
-    return InlineKeyboardMarkup(keyboard)
+    query.answer()
 
-
-N_LINES = 4
-USE_USER_TEXT = False
+    if query.data == 'generate_new':
+        first_line = (query.message.text_markdown.split('\n')[0])[1:-1]
+        query.edit_message_text(text=get_poem(first_line), reply_markup=keyboard_markup, parse_mode=ParseMode.MARKDOWN)
+    elif query.data == 'save':
+        query.edit_message_reply_markup(reply_markup=None)
 
 
 def get_poem(first_line=None):
-    gen_lines = markov_generate()
+    words = list(first_line.split())
+    words = words[-min(2, len(words)):]
+    while len(words) < 2:
+        words = [words[0]] + words
 
-    if first_line is None:
-        return gen_lines
-    else:
-        return f'*{first_line}*\n{gen_lines}'
+    gen_lines = markov_generate(words)
+
+    return f'*{first_line}*\n{gen_lines}'
 
 
 def new_poem_handler(update: Update, context: CallbackContext):
-    text = get_poem(update.message.text if USE_USER_TEXT else None)
+    text = get_poem(update.message.text)
 
-    update.message.reply_text(text)
+    update.message.reply_text(text, reply_markup=keyboard_markup, parse_mode=ParseMode.MARKDOWN)
 
 
 def main() -> None:
@@ -53,6 +62,7 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler(['start', 'help'], help_handler))
+    dispatcher.add_handler(CallbackQueryHandler(callback_handler))
     dispatcher.add_handler(MessageHandler(Filters.text, new_poem_handler))
 
     updater.start_polling()
